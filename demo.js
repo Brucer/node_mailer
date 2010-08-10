3468d01f6c1511e2e82cdec01da0f1a8db86414a
bruce rogers <Brucer@gmail.com>
2010-08-10 16:33:02 -0700

    removed encode64 - users node buffer.toString('base64') instead removed extraneous code (no need to email obama by default :)

--- demo.js
+++ demo.js
@@ -1,23 +1,14 @@
 var email = require("mailer");
 
-
-var mimes = [ {data:"mime text message"}, {filename:"test.png"}, {filename:"test.jpg"} ];
-for(var i in mimes)
-{
-	if (mimes[i].filename)
-		mimes[i].data = fs.readFileSync(mimes[i].filename, "binary");
-}
+var mime = {filename:"test.jpg"};
+	mime.data = fs.readFileSync(mime.filename);
 
 email.send({
   host : "localhost",              // smtp server hostname
   port : "25",                     // smtp server port
-  domain : "localhost",            // domain used by client to identify itself to server
-  authentication : "login",        // auth login is supported; anything else is no auth
-  username : "dXNlcm5hbWU=",       // Base64 encoded username
-  password : "cGFzc3dvcmQ=",       // Base64 encoded password
-  to : "marak.squires@gmail.com",
-  from : "obama@whitehouse.gov",
+	from : "email@corp.com",
+	to : "person@corp.com",
   subject : "node_mailer test email",
   body : "hello this is a test email from the node_mailer",
-	mimes : mimes
+	mimes : [mime]
 });
